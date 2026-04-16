0a9a36c4539d36bf0d6c88b451a6ea43893fc281
Ble <xbledix@gmail.com>
2026-04-16 15:51:46 +0200

    Reduce tab height by 4px (14px -> 10px)

--- crates/tp-gui/src/theme.rs
+++ crates/tp-gui/src/theme.rs
@@ -599,7 +599,7 @@ notebook.workspace-tabs > header > tabs > tab {
   border-radius: 0;
   margin: 0;
   border-spacing: 0;
-  min-height: 14px;
+  min-height: 10px;
   background-color: transparent;
   padding-top: 0;
   padding-bottom: 0;
